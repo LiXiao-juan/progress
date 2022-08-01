e49cdb35a323195294580002303d2dd6db239213
Li <1627843848@qq.com>
2022-08-01 10:00:03 +0800

    完成进度条组件封装

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Progress pct="20%"></Progress>
+    <br/>
+    <Progress pct="80%"></Progress>
+    <br>
+    <Progress pct="100%"></Progress>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Progress from '@/components/progress.vue'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    Progress,
+  },
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {},
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped lang="less"></style>
---
+++ src/components/progress.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="outer">
+    <div class="inside" :style="{ width: pct }" :class="{ finish: pct === '100%'}">{{ pct }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {}
+  },
+  props: {
+    pct: {
+      type: Number,
+      default: 0,
+    },
+  },
+  created() {},
+  methods: {},
+}
+</script>
+
+<style scoped lang="less">
+.outer {
+  height: 40px;
+  width: 300px;
+  background-color: aqua;
+  border: 1px solid #000;
+}
+.inside {
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+  color: #fff;
+  background-color: purple;
+}
+.finish{
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+  color: #fff;
+  background-color: greenyellow;
+}
+</style>
